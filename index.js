513920d80ccc5c8139a8ab8715acad8f7ec93315
Rostyslav Stepanyak <rostyslav.stepanyak@darkmatter.ae>
2017-03-13 01:57:23 +0400

    update port

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express')
  
-var app = express()
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
  
 app.get('/notes', function(req, res) {
     var from = req.query.from;
     res.header('Content-Type','text/xml').send('<yo>' + from + '</yo>');
 })
  
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
